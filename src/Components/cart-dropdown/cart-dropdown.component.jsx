4e0c4f7831a04077203e25b97979a3c697e44060
Yash Gupta <yashguptayashu2003@gmail.com>
2023-10-28 11:33:40 +0530

    added new cart feature

--- src/Components/cart-dropdown/cart-dropdown.component.jsx
+++ src/Components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,26 @@
 import Button from '../button/button.component.jsx';
+import CartItem from '../cart-item/cart-item.component.jsx';
+import { CartContext } from '../contexts/cart.context.jsx';
 import './cart-dropdown.styles.scss';
+import { useContext } from 'react';
+
 
 const CartDropDown = () => {
+
+    const { cartItems } = useContext(CartContext);
+
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
-                <Button>Go to Cart</Button>
+                {
+                cartItems.map( (item) => 
+                    (
+                        <CartItem key={item.id} cartItem={item}/>
+                    )
+                )
+                }
             </div>
+            <Button>CheckOut</Button>
         </div>
     )
 }
