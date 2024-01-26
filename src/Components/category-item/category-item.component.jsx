767a78587b68fc184a21914f4fc6615a13fb02dd
Yash Gupta <yashguptayashu2003@gmail.com>
2024-01-26 19:26:02 +0530

    home component links added

--- src/Components/category-item/category-item.component.jsx
+++ src/Components/category-item/category-item.component.jsx
@@ -1,17 +1,21 @@
 import "./category-item.styles.scss";
+import { useNavigate } from "react-router-dom";
 
 const CategoryItem = ({category})=> {
 
-    const {imageUrl , title} = category;
+    const {imageUrl , title, route} = category;
+    const navigate = useNavigate();
+
+    const onNavigationHandler = () => navigate(route);
 
     return (
-        <div className="category-container">
+        <div className="category-container" onClick={onNavigationHandler}>
         <div className="background-image" style={{
           backgroundImage: `url(${imageUrl})`
         }}/>
         <div className="category-body-container">
-          <h2>{title}</h2>
-          <p>Shop Now</p>
+            <h2>{title}</h2>
+            <p>Shop Now</p>
         </div>
       </div>
     )
--- src/Components/category-preview/category-preview.component.jsx
+++ src/Components/category-preview/category-preview.component.jsx
@@ -1,10 +1,13 @@
 import ProductCard from "../product-card/productCard.component";
 import './category-preview.styles.scss';
+import { Link } from "react-router-dom";
 
 const CategoryPreview = ({title, products})=> {
     return (
         <div className="category-preview-container">
+            <Link to={`/shop/${title}`}>
             <h2 className="title"><span>{title.toUpperCase()}</span></h2>
+            </Link>
             <div className="preview">
                 {
                     products.filter((_,idx)=> idx< 4).map((product)=>(
